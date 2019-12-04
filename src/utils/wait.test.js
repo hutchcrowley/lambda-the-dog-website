70f70f90625326d109d91e5ee30141195bbd07fa
brian kirkby <bkirkby@instructure.com>
2019-12-03 21:46:12 -0700

    webpt9 project

---
+++ src/utils/wait.test.js
@@ -0,0 +1,38 @@
+import wait from "./wait";
+
+// async test with the done method way
+// test("wait for promise to resolve", done => {
+//   wait(3).then(result => {
+//     expect(result).toBe("hurray!");
+//     done();
+//   });
+// });
+
+// async test with the return Promise way
+// test("wait for promise to resolve", () => {
+//   return wait(3).then(result => {
+//     expect(result).toBe("hurray!");
+//   });
+// });
+
+// async test with async/await
+// test("wait for promise to resolve", async () => {
+//   const result = await wait(3);
+//   expect(result).toBe("hurray!");
+// });
+jest.useFakeTimers();
+
+test("wait for promise to resolve", async () => {
+  const spy = jest.fn();
+
+  const waitFn = wait(3, spy);
+
+  jest.runAllTimers();
+
+  const result = await waitFn;
+
+  expect(result).toBe("hurray!");
+
+  expect(spy).toHaveBeenCalledWith("resolve finished");
+  expect(spy).toHaveBeenCalledTimes(1);
+});
